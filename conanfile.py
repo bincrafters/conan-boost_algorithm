683c2fd32e020698057c39300cc574b84822bbfe
Rene Rivera <grafikrobot@gmail.com>
2018-01-29 09:53:49 -0600

    Merge.

--- conanfile.py
+++ conanfile.py
@@ -17,23 +17,23 @@ class BoostAlgorithmConan(ConanFile):
         self.info.header_only()
 
     requires = (
-        "boost_package_tools/1.66.0@bincrafters/stable",
-        "boost_array/1.66.0@bincrafters/stable",
-        "boost_assert/1.66.0@bincrafters/stable",
-        "boost_bind/1.66.0@bincrafters/stable",
-        "boost_concept_check/1.66.0@bincrafters/stable",
-        "boost_config/1.66.0@bincrafters/stable",
-        "boost_core/1.66.0@bincrafters/stable",
-        "boost_exception/1.66.0@bincrafters/stable",
-        "boost_function/1.66.0@bincrafters/stable",
-        "boost_iterator/1.66.0@bincrafters/stable",
-        "boost_mpl/1.66.0@bincrafters/stable",
-        "boost_range/1.66.0@bincrafters/stable",
-        "boost_static_assert/1.66.0@bincrafters/stable",
-        "boost_throw_exception/1.66.0@bincrafters/stable",
-        "boost_tuple/1.66.0@bincrafters/stable",
-        "boost_type_traits/1.66.0@bincrafters/stable",
-        "boost_unordered/1.66.0@bincrafters/stable"
+        "boost_package_tools/1.66.0@bincrafters/testing",
+        "boost_array/1.66.0@bincrafters/testing",
+        "boost_assert/1.66.0@bincrafters/testing",
+        "boost_bind/1.66.0@bincrafters/testing",
+        "boost_concept_check/1.66.0@bincrafters/testing",
+        "boost_config/1.66.0@bincrafters/testing",
+        "boost_core/1.66.0@bincrafters/testing",
+        "boost_exception/1.66.0@bincrafters/testing",
+        "boost_function/1.66.0@bincrafters/testing",
+        "boost_iterator/1.66.0@bincrafters/testing",
+        "boost_mpl/1.66.0@bincrafters/testing",
+        "boost_range/1.66.0@bincrafters/testing",
+        "boost_static_assert/1.66.0@bincrafters/testing",
+        "boost_throw_exception/1.66.0@bincrafters/testing",
+        "boost_tuple/1.66.0@bincrafters/testing",
+        "boost_type_traits/1.66.0@bincrafters/testing",
+        "boost_unordered/1.66.0@bincrafters/testing"
     )
 
     # BEGIN
@@ -41,7 +41,7 @@ class BoostAlgorithmConan(ConanFile):
     description = "Please visit http://www.boost.org/doc/libs/1_66_0"
     license = "BSL-1.0"
     short_paths = True
-    build_requires = "boost_generator/1.66.0@bincrafters/stable"
+    build_requires = "boost_generator/1.66.0@bincrafters/testing"
 
     def package_id(self):
         getattr(self, "package_id_additional", lambda:None)()
