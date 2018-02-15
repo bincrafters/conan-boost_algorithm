a1c1c5fcc2ad06e3945b1d2c7f6536b8c3c65e98
Eric Pederson <eric.pederson@tradeweb.com>
2018-02-15 16:00:12 -0500

    Add test_package

---
+++ test_package/test_package.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <boost/algorithm/string.hpp>
+
+int main()
+{
+    std::vector<std::string> list;
+    list.push_back("Hello");
+    list.push_back("World!");
+
+    std::string joined = boost::algorithm::join(list, ", ");
+    std::cout << joined << '\n';
+}
