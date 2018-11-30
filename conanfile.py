5840b251909c8d4e97cf0ede15e11ba0deef6f64
Rene Rivera <grafikrobot@gmail.com>
2018-11-29 21:59:07 -0600

    Initial 1.68.0 [skip ci]

--- conanfile.py
+++ conanfile.py
@@ -4,31 +4,12 @@
 from conans import python_requires
 
 
-base = python_requires("boost_base/1.67.0@bincrafters/testing")
+base = python_requires("boost_base/1.68.0@bincrafters/testing")
 
 class BoostAlgorithmConan(base.BoostBaseConan):
     name = "boost_algorithm"
     url = "https://github.com/bincrafters/conan-boost_algorithm"
     lib_short_names = ["algorithm"]
     header_only_libs = ["algorithm"]
-    b2_requires = [
-        "boost_array",
-        "boost_assert",
-        "boost_bind",
-        "boost_concept_check",
-        "boost_config",
-        "boost_core",
-        "boost_exception",
-        "boost_function",
-        "boost_iterator",
-        "boost_mpl",
-        "boost_range",
-        "boost_regex",
-        "boost_static_assert",
-        "boost_throw_exception",
-        "boost_tuple",
-        "boost_type_traits",
-        "boost_unordered"
-    ]
-
-
+    cycle_group = "boost_cycle_group_a"
+    b2_requires = ["boost_cycle_group_a"]
